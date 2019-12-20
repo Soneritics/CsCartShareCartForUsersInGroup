bcf1676ff08f702bb0f88da249c4f6b58bc084e4
Jordi Jolink <info@soneritics.nl>
2019-12-20 09:25:21 +0100

    Add method to fetch a code from user data (for use in order part, etc.)

--- app/addons/soneritics_sharecart/func.php
+++ app/addons/soneritics_sharecart/func.php
@@ -1,2 +1,20 @@
 <?php
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+function fn_soneritics_sharecart_getCodeFromUserData(array $userData): string
+{
+    if (!empty($userData['fields'])) {
+        $code = SoneriticsShareCartSettingsFactory::create()->getPraktijkcode();
+        $profileFieldIds = (new SoneriticsShareCartProfileField($code))->getIds();
+
+        if (!empty($profileFieldIds)) {
+            foreach ($profileFieldIds as $profileFieldId) {
+                if (!empty($userData['fields'][$profileFieldId])) {
+                    return $userData['fields'][$profileFieldId];
+                }
+            }
+        }
+    }
+
+    return '';
+}
--- app/addons/soneritics_sharecart/init.php
+++ app/addons/soneritics_sharecart/init.php
@@ -7,3 +7,5 @@ require_once __DIR__ . '/Model/SoneriticsShareCartOverviewLine.php';
 require_once __DIR__ . '/Business/SoneriticsShareCartSettingsFactory.php';
 require_once __DIR__ . '/Business/SoneriticsShareCartOverviewGenerator.php';
 require_once __DIR__ . '/Repository.php';
+
+//fn_register_hooks();
